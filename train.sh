41151066a5d8febaee5c07cb5bfb007b75a456ec
k3vkoh <kev33koh@gmail.com>
2023-11-14 18:58:43 -0500

    train model

---
+++ train.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+modelName=''
+
+cd tesstrain
+
+TESSDATA_PREFIX=../tesseract/tessdata gmake training MODEL_NAME=$modelName START_MODEL= eng TESSDATA=../tesseract/tessdata MAX_ITERATIONS=1000 
+
+mv data/$modelName.traineddata /usr/local/share/tessdata/
