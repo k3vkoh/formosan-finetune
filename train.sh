294b4d7881f503fd9af4d0f2b3103dd2d9810b82
k3vkoh <kev33koh@gmail.com>
2023-11-15 10:07:19 -0500

    fixed bug: removed space

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-modelName=''
+modelName='favorlang'
 
 cd src/tesstrain
 
-TESSDATA_PREFIX=../tesseract/tessdata gmake training MODEL_NAME=$modelName START_MODEL= eng TESSDATA=../tesseract/tessdata MAX_ITERATIONS=1000 
+TESSDATA_PREFIX=../tesseract/tessdata gmake training MODEL_NAME=$modelName START_MODEL=eng TESSDATA=../tesseract/tessdata MAX_ITERATIONS=1000 
 
 mv data/$modelName.traineddata /usr/local/share/tessdata/
